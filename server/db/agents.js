ee94d0430b553bc27fca1330712534a7c083b28f
pradeepmvn <mvnpradeep@gmail.com>
2018-07-31 16:07:10 -0400

    Fix #93. Importing NLU data with Entities.

--- server/db/agents.js
+++ server/db/agents.js
@@ -40,30 +40,27 @@ function uploadAgentFromFile(req, res, next) {
     expressions_arr.push(expressionObj);
     intents_map.set(nlu_data_arr[i].intent, expressions_arr);
   }
-  console.log("Done Iterations. Inserting Entites now ... with json");
+  console.log("Done Iterations. Inserting data now.");
 
+  // data = as returned from the transaction's callback
   db.tx(function (t) {
-    var entity_queries_arr = [];
-    entities_set.forEach(function (entity) {
-      var entity_query = t.one('insert into entities(entity_name) values($1) RETURNING entity_id', entity)
-        .then(function (return_entity) {
-          console.log("Entity Inserted. Updating map for " + entity);
-          entities_map.set(entity, return_entity.entity_id);
-        }).catch(function (err) {
-          console.log("Error occured while inserting entities: " + err);
+    // t.ctx = transaction context object
+    return t.one('insert into agents(agent_name) values($1) RETURNING agent_id', [req.body.agent_name])
+      .then(agent => {
+        console.log("Agent Inserted. Inserting Entites First. These ids are needed for Intents.");
+        var entity_queries_arr = [];
+        entities_set.forEach(function (entity) {
+          var entity_query = t.one('insert into entities(entity_name, agent_id, slot_data_type) values($1,$2,$3) RETURNING entity_id', [entity,agent.agent_id,'NOT_USED'])
+            .then(function (return_entity) {
+              console.log("Entity Inserted. Updating map for " + entity);
+              entities_map.set(entity, return_entity.entity_id);
+            }).catch(function (err) {
+              console.log("Error occured while inserting entities: " + err);
+            });
+          entity_queries_arr.push(entity_query);
         });
-      entity_queries_arr.push(entity_query);
-    });
-    return t.batch(entity_queries_arr);
-  }).then(data => {
-    // success
-    // data = as returned from the transaction's callback
-    console.log("All Entites Inserted. Proceeding with other data inserts");
-    db.tx(function (t) {
-      // t.ctx = transaction context object
-      return t.one('insert into agents(agent_name) values($1) RETURNING agent_id', [req.body.agent_name])
-        .then(agent => {
-          console.log("Agent Inserted");
+        return t.batch(entity_queries_arr)
+        .then(dataAfterAgentAndEntities => {
           var intents_query_arr = [];
           intents_map.forEach(function (expressionsArray, key, map) {
             console.log("Inserting Intent " + key);
@@ -82,7 +79,7 @@ function uploadAgentFromFile(req, res, next) {
                           //updated entityid
                           paramObj.entity_id = entities_map.get(paramObj.entity);
                         }
-                        console.log("Inserting Parameter for " + paramObj.parameter_value + " ,Mapping to Entity: " + paramObj.entity + " ,with key" + paramObj.entity_id);
+                        console.log("Inserting Parameter for " + paramObj.parameter_value + " , Mapping to Entity: " + paramObj.entity + " ,with key: " + paramObj.entity_id);
                         var params_query = t.none('insert into parameters (expression_id, parameter_end, parameter_start, parameter_value,entity_id) values($1,$2,$3,$4,$5)',
                           [expression.expression_id, paramObj.end, paramObj.start, paramObj.parameter_value, paramObj.entity_id]);
                         parameters_query_arr.push(params_query);
@@ -97,34 +94,16 @@ function uploadAgentFromFile(req, res, next) {
           });
           return t.batch(intents_query_arr);
         });
-    }).then(data => {
-      // success
-      // data = as returned from the transaction's callback
-      console.log("All Data inserted");
-      return res.status(200).json({});
-    })
-      .catch(error => {
-        // error should rollback
-        //delete entites as well
-        console.log("Error occured. Rollbacking all: " + error);
-        var entityIds;
-        entities_map.forEach(function (value, key, map) {
-          entityIds = entityIds + ', ' + value;
-        });
-        entityIds = entityIds.substring(1, entityIds.length);
-        db.none('delete from entities where entity_id in (' + entityIds + ')')
-          .then(function () {
-            console.log("Entites deleted successfully");
-          })
-          .catch(function (err) {
-            return next(err);
-          });
-        return res.status(500).json({ "Error": "Error Occurred" });
       });
+  }).then(data => {
+    // success. All Data inserted.
+    console.log("----- All Data inserted. Sending response back --------");
+    return res.status(200).json({});
   }).catch(error => {
-    console.log("Error when uploading entites" + error);
-    return res.status(500).json({ "Error": "Error Occurred" });
-  });
+      // error should rollback delete entites as well
+      console.log("Error occured. Rollbacking all: " + error);
+      return res.status(500).json({ "Error": "Error Occurred" });
+    });
 }
 
 function getAllAgents(req, res, next) {
@@ -186,13 +165,11 @@ function removeAgent(req, res, next) {
   var agentID = parseInt(req.params.agent_id);
   db.result('delete from agents where agent_id = $1', agentID)
     .then(function (result) {
-      /* jshint ignore:start */
       res.status(200)
         .json({
           status: 'success',
           message: 'Removed ${result.rowCount}'
         });
-      /* jshint ignore:end */
     })
     .catch(function (err) {
       return next(err);
