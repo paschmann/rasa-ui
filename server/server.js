5844986aecef32af1d483129d82e751361844366
pradeepmvn <mvnpradeep@gmail.com>
2018-10-09 15:24:36 -0400

    Added more comment on socket communication.

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ app.set("socketCache",socketCache);
 // Socket.io Communication
 io.sockets.on('connection', function (socket) {
   var jwt0='';
-  console.log("All Cookies: " + socket.request.headers.cookie);
+  console.log("All Cookies for Socket Connection: " + socket.request.headers.cookie);
   try{
     var cookieArr =socket.request.headers.cookie.split(";");
     for (var i=0;i<cookieArr.length;i++){
@@ -86,15 +86,13 @@ io.sockets.on('connection', function (socket) {
         break;
       }
     }
-  console.log("Adding Socket to Sockets List");
+  console.log("Socket Connection Request for jwt0: " +jwt0);
   app.get("socketCache").set(jwt0, socket);
   }catch (err) {
     console.log('Problem when parsing the cookies. Ignoring socket' + err);
   }
-
   socket.on('disconnect', function(){
-    console.log("Disconnecting All Cookies: " + socket.request.headers.cookie);
-    console.log('Socket disconnected');
+    console.log("All Cookies for Socket disconnect: " + socket.request.headers.cookie);
     var discjwt0='';
     try{
       var cookieArr =socket.request.headers.cookie.split(";");
@@ -104,8 +102,8 @@ io.sockets.on('connection', function (socket) {
           break;
         }
       }
-      console.log("Removing SOCKET: "+discjwt0+" from List: "+ JSON.stringify(app.get("socketCache").keys()));
-      app.get("socketCache").del(jwt0);
+      console.log("Socket Disconnect Request. Removing: "+discjwt0+" from List: "+ JSON.stringify(app.get("socketCache").keys()));
+      app.get("socketCache").del(discjwt0);
     }catch (err) {
       console.log('Problem when parsing the cookies. Unable to delete socket' + err);
     }
