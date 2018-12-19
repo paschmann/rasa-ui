716b5b243f0622afa5f101af57f4515a5dea7302
Axel Laurelut <axel.laurelut@cacd2.fr>
2018-12-19 17:26:38 +0000

    Cbt 33 us historique conversations

--- resources/database/dbcreate.sql
+++ resources/database/dbcreate.sql
@@ -408,7 +408,37 @@ WITH (
 )
 TABLESPACE pg_default;
 
+-- nlu_parse_log correspondance table with entities table 
+CREATE TABLE messages_entities
+(
+  message_id integer,
+  entity_id integer,
+  entity_start integer NOT NULL,
+  entity_end integer NOT NULL,
+  entity_confidence integer NOT NULL,
+  CONSTRAINT message_entity_pkey PRIMARY KEY (message_id, entity_id, entity_start, entity_end),
+  CONSTRAINT message_fkey FOREIGN KEY (message_id) REFERENCES messages (messages_id) ON DELETE CASCADE,
+  CONSTRAINT entity_fkey FOREIGN KEY (entity_id) REFERENCES entities (entity_id) ON DELETE CASCADE
+)
+WITH (
+  OIDS = FALSE
+)
+TABLESPACE pg_catalog;  
 
+-- messages correspondance with intents table 
+CREATE TABLE messages_intents
+(
+  message_id integer,
+  intent_id integer,
+  intent_confidence integer NOT NULL,
+  CONSTRAINT message_intent_pkey PRIMARY KEY (message_id, intent_id),
+  CONSTRAINT message_fkey FOREIGN KEY (message_id) REFERENCES messages (messages_id) ON DELETE CASCADE,
+  CONSTRAINT intent_fkey FOREIGN KEY (intent_id) REFERENCES intents (intent_id) ON DELETE CASCADE
+)
+WITH (
+  OIDS = FALSE
+)
+TABLESPACE pg_catalog;  
 
 /* Views */
 CREATE OR REPLACE VIEW intents_most_used AS
