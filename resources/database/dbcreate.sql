8e01f3b32390fba2c1c167dbc44b881b3081e0b6
Romain Dauvert <romain@tekhne.fr>
2018-12-21 11:07:33 +0100

    Enforce use of rasa_ui schema without server restart
    Remove explicit rasa_ui reference for responses response_type foreign key

--- resources/database/dbcreate.sql
+++ resources/database/dbcreate.sql
@@ -11,6 +11,8 @@ BEGIN
   EXECUTE 'ALTER DATABASE '|| current_database()||' set search_path = ''rasa_ui'', ''$user'', ''public''';
 END $$;
 
+SET search_path TO rasa_ui;
+
 /* Sequences */
 
 CREATE SEQUENCE agentidgen
@@ -300,7 +302,7 @@ CREATE TABLE responses
   CONSTRAINT response_pkey PRIMARY KEY (response_id),
   CONSTRAINT intent_fkey FOREIGN KEY (intent_id) REFERENCES intents (intent_id) ON DELETE CASCADE,
   CONSTRAINT action_fkey FOREIGN KEY (action_id) REFERENCES actions (action_id) ON DELETE CASCADE,
-  CONSTRAINT responses_response_type_fkey FOREIGN KEY (response_type) REFERENCES rasa_ui.response_type (response_type_id) ON DELETE CASCADE
+  CONSTRAINT responses_response_type_fkey FOREIGN KEY (response_type) REFERENCES response_type (response_type_id) ON DELETE CASCADE
 )
 WITH (
   OIDS = FALSE
