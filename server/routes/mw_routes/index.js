d193b81f0cac88bff2a07fe0139832822b71891c
Romain Dauvert <romain.dauvert@cacd2.fr>
2018-12-21 12:27:18 +0000

    Cbt 162 us fix rasa ui

---
+++ server/routes/mw_routes/index.js
@@ -0,0 +1,10 @@
+/*
+    load core router
+*/
+const CoreServerV10 = require('./core_server_v10');
+const CoreServerV12 = require('./core_server_v12');
+
+module.exports = {
+    CoreServerV10,
+    CoreServerV12
+};
