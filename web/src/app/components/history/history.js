7a660961032e12496d8cfc688786fbdee4ea92da
Axel Laurelut <axellaurelut@MacBook-Pro-de-Axel.local>
2019-02-01 15:49:33 +0100

    on history page, select first agent in list by default

--- web/src/app/components/history/history.js
+++ web/src/app/components/history/history.js
@@ -1,25 +1,26 @@
-angular.module('app').controller('HistoryController', HistoryController);
+angular.module("app").controller("HistoryController", HistoryController);
 
 function HistoryController($scope, $http, $location, Agent, appConfig) {
   $scope.conversations = [];
-  $scope.selectedAgentId = '';
+  $scope.selectedAgent = "";
   $scope.itemsPerPage = 20;
   $scope.currentPage = 1;
 
   Agent.query(function(data) {
     $scope.agentList = data;
     if ($scope.agentList.length > 0) {
-      $scope.selectedAgentId = data[0].agent_id;
+      $scope.selectedAgent = data[0];
       $scope.loadAgentConversations();
     }
   });
 
   $scope.loadAgentConversations = function() {
     $http({
-      method: 'GET',
+      method: "GET",
       url: `${appConfig.api_endpoint_v2}/agent/${
-        $scope.selectedAgentId
-      }/messages?itemsPerPage=${$scope.itemsPerPage}&page=${$scope.currentPage}`}).then(
+        $scope.selectedAgent.agent_id
+      }/messages?itemsPerPage=${$scope.itemsPerPage}&page=${$scope.currentPage}`
+    }).then(
       function(response) {
         $scope.totalItems = response.data.meta.total;
         $scope.conversations = response.data.conversations;
@@ -36,6 +37,6 @@ function HistoryController($scope, $http, $location, Agent, appConfig) {
   };
 
   $scope.goToConversation = function(userId) {
-    $location.path(`/conversation/${$scope.selectedAgentId}/${userId}`);
+    $location.path(`/conversation/${$scope.selectedAgent.agent_id}/${userId}`);
   };
 }
