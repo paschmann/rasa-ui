fc8ecc836a0f3de99d5ecb7abb79f331e968e682
Axel Laurelut <axellaurelut@MacBook-Pro-de-Axel.local>
2019-02-01 15:24:38 +0100

    logout user on jwt token invalid

--- web/src/app/app.js
+++ web/src/app/app.js
@@ -1,21 +1,21 @@
 var app = angular
-  .module('app', [
-    'ngCookies',
-    'btford.socket-io',
-    'ng-jsyaml',
-    'ngSanitize',
-    'ngFileUpload',
-    'angularUtils.directives.dirPagination',
-    'ngRoute',
-    'chart.js',
-    'ngResource',
-    'ngStorage',
-    'ngTagsInput',
-    'jsonFormatter',
-    'angularModalService',
-    'AdalAngular'
+  .module("app", [
+    "ngCookies",
+    "btford.socket-io",
+    "ng-jsyaml",
+    "ngSanitize",
+    "ngFileUpload",
+    "angularUtils.directives.dirPagination",
+    "ngRoute",
+    "chart.js",
+    "ngResource",
+    "ngStorage",
+    "ngTagsInput",
+    "jsonFormatter",
+    "angularModalService",
+    "AdalAngular"
   ])
-  .factory('mySocket', function(socketFactory) {
+  .factory("mySocket", function(socketFactory) {
     return socketFactory({
       ioSocket: window.io.connect()
     });
@@ -27,27 +27,28 @@ var app = angular
     if ($sessionStorage.jwt) {
       $rootScope.authenticated = true;
       $http.defaults.headers.common.Authorization =
-        'Bearer ' + $sessionStorage.jwt;
+        "Bearer " + $sessionStorage.jwt;
     } else {
       //show login page
       $rootScope.authenticated = false;
-      $rootScope.$broadcast('INVALID_JWT_TOKEN');
+      $rootScope.$broadcast("INVALID_JWT_TOKEN");
     }
-    $rootScope.$on('USER_AUTHENTICATED', function(event) {
+    $rootScope.$on("USER_AUTHENTICATED", function(event) {
       $rootScope.authenticated = true;
       $http.defaults.headers.common.Authorization =
-        'Bearer ' + $sessionStorage.jwt;
+        "Bearer " + $sessionStorage.jwt;
     });
 
-    $rootScope.$on('INVALID_JWT_TOKEN', function(event) {
+    $rootScope.$on("INVALID_JWT_TOKEN", function(event) {
       $rootScope.authenticated = false;
       $sessionStorage.$reset();
+      adalAuthenticationService.logOut();
     });
   });
 
 angular
-  .module('app')
-  .controller('appCtrl', function(
+  .module("app")
+  .controller("appCtrl", function(
     $rootScope,
     $scope,
     $route,
@@ -70,31 +71,31 @@ angular
 
     $scope.formData = {};
 
-    $scope.$on('setAlertText', function(event, alert_text) {
-      $('#alertTextDiv').addClass('show');
+    $scope.$on("setAlertText", function(event, alert_text) {
+      $("#alertTextDiv").addClass("show");
       $scope.alert_text = alert_text;
       $timeout(function() {
-        $('#alertTextDiv').removeClass('show');
+        $("#alertTextDiv").removeClass("show");
       }, 2000);
     });
 
     $scope.loginUser = function(user) {
       $http
-        .post(appConfig.api_endpoint_v2 + '/auth', JSON.stringify(user))
+        .post(appConfig.api_endpoint_v2 + "/auth", JSON.stringify(user))
         .then(
           function(response) {
             // success callback
             $sessionStorage.jwt = response.data.token;
-            $cookies.put('loggedinjwt', $sessionStorage.jwt);
-            $rootScope.$broadcast('USER_AUTHENTICATED');
+            $cookies.put("loggedinjwt", $sessionStorage.jwt);
+            $rootScope.$broadcast("USER_AUTHENTICATED");
           },
           function(errorResponse) {
             // failure callback
-            $('#alertTextDiv').addClass('show');
+            $("#alertTextDiv").addClass("show");
             $scope.alert_text =
-              'Invalid Username and Password. Please try again.';
+              "Invalid Username and Password. Please try again.";
             $timeout(function() {
-              $('#alertTextDiv').removeClass('show');
+              $("#alertTextDiv").removeClass("show");
             }, 2000);
           }
         );
@@ -104,14 +105,14 @@ angular
     if (appConfig.adalauthentication) {
       // this is referencing adal module to do login
       //userInfo is defined at the $rootscope with adalAngular module
-      $scope.testMessage = '';
+      $scope.testMessage = "";
       $scope.init = function() {
-        $scope.testMessage = '';
+        $scope.testMessage = "";
       };
 
       $scope.logout = function() {
         adalAuthenticationService.logOut();
-        $rootScope.$broadcast('INVALID_JWT_TOKEN');
+        $rootScope.$broadcast("INVALID_JWT_TOKEN");
       };
 
       $scope.login = function() {
@@ -119,27 +120,27 @@ angular
       };
 
       // optional
-      $scope.$on('adal:loginSuccess', function() {
-        $scope.testMessage = 'loginSuccess';
+      $scope.$on("adal:loginSuccess", function() {
+        $scope.testMessage = "loginSuccess";
 
         // Inject Azure Token_ID as JWT Token
         const clientID = appConfig.adalclientid;
         $sessionStorage.jwt = adalAuthenticationService.getCachedToken(
           clientID
         );
-        $cookies.put('loggedinjwt', $sessionStorage.jwt);
-        $rootScope.$broadcast('USER_AUTHENTICATED');
+        $cookies.put("loggedinjwt", $sessionStorage.jwt);
+        $rootScope.$broadcast("USER_AUTHENTICATED");
       });
 
       // optional
-      $scope.$on('adal:loginFailure', function() {
-        $scope.testMessage = 'loginFailure';
-        $location.path('/login');
+      $scope.$on("adal:loginFailure", function() {
+        $scope.testMessage = "loginFailure";
+        $location.path("/login");
       });
 
       // optional
-      $scope.$on('adal:notAuthorized', function(event, rejection, forResource) {
-        $scope.testMessage = 'It is not Authorized for resource:' + forResource;
+      $scope.$on("adal:notAuthorized", function(event, rejection, forResource) {
+        $scope.testMessage = "It is not Authorized for resource:" + forResource;
       });
     }
   });
