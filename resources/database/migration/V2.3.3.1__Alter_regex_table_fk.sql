3fbdf84ce8c81ceb5b5f4cc4cab5c3e7d536d09e
pradeepmvn <mvnpradeep@gmail.com>
2019-02-10 19:29:22 -0500

    Merged changes from multiple db versions

--- resources/database/migration/V2.3.3.1__Alter_regex_table_fk.sql
+++
@@ -1,4 +0,0 @@
-SET search_path TO rasa_ui;
-
-ALTER TABLE IF EXISTS regex ADD COLUMN agent_id INTEGER;
-ALTER TABLE IF EXISTS regex ADD CONSTRAINT agent_fkey FOREIGN KEY (agent_id) REFERENCES agents (agent_id) ON DELETE CASCADE;
