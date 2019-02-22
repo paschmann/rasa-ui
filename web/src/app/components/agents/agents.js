fd370089938a3b179158fd6f72d76c0c7a9fc486
pradeepmvn <mvnpradeep@gmail.com>
2019-02-21 23:25:07 -0500

    minor look and feel changes

--- web/src/app/components/agents/agents.js
+++ web/src/app/components/agents/agents.js
@@ -2,13 +2,19 @@ angular
 .module('app')
 .controller('AgentsController', AgentsController);
 
-function AgentsController($scope, $rootScope, Agent) {
+function AgentsController($scope, $rootScope, Agent,$http) {
   Agent.query(function(data) {
       $scope.agentList = data;
   });
 
   $scope.agentEncoded = function(agent) {
-    return btoa(JSON.stringify({agent_id:agent.agent_id, agent_name:agent.agent_name, client_secret: agent.client_secret_key}));
+    $http({method: 'GET', url: appConfig.api_endpoint_v2 + '/rasa/url'}).then(
+      function(response){
+        return btoa(JSON.stringify({agent_id:agent.agent_id, agent_name:agent.agent_name, client_secret: agent.client_secret_key,nlu_endpoint:response.data}));
+      },
+      function(errorResponse){
+        console.log("Error Message while Getting Messages." + errorResponse);
+      });
   }
 
   $scope.editAgentInfo = function(agent) {
