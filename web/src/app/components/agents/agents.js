7329f2bf8a191404c0aa58fd9255acd907c98de7
Mamillapalli <pmamill@LAMU02XW3B2JGH6.uhc.com>
2019-03-01 17:32:22 -0500

    Updated demo page to have nlu URL.

--- web/src/app/components/agents/agents.js
+++ web/src/app/components/agents/agents.js
@@ -2,21 +2,22 @@ angular
 .module('app')
 .controller('AgentsController', AgentsController);
 
-function AgentsController($scope, $rootScope, Agent,$http,appConfig) {
+function AgentsController($scope, $rootScope, Agent,$http,appConfig,$window) {
   Agent.query(function(data) {
       $scope.agentList = data;
   });
 
-  $scope.agentEncoded = function(agent) {
+  $scope.botDemoClick = function(agent){
     $http({method: 'GET', url: appConfig.api_endpoint_v2 + '/rasa/url'}).then(
       function(response){
-        return btoa(JSON.stringify({agent_id:agent.agent_id, agent_name:agent.agent_name, client_secret: agent.client_secret_key,nlu_endpoint:response.data}));
+        $window.open('/bot-demo?id='+btoa(JSON.stringify({agent_id:agent.agent_id, agent_name:agent.agent_name, client_secret: agent.client_secret_key,nlu_endpoint:response.data.url})));
       },
       function(errorResponse){
         console.log("Error Message while Getting Messages." + errorResponse);
       });
+      
+  
   }
-
   $scope.editAgentInfo = function(agent) {
     Agent.update({ agent_id:agent.agent_id }, agent).$promise.then(function() {
       $('#'+agent.agent_id).collapse('hide');
