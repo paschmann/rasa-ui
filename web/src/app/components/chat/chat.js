cd9b31e7a3dc786a1ddecbeabd047c2ad0cbd80a
Paul Aschmann <paul@paups.com>
2019-10-16 11:26:33 -0400

    Minor changes to chat interface

--- web/src/app/components/chat/chat.js
+++ web/src/app/components/chat/chat.js
@@ -30,7 +30,7 @@ function ChatController($scope, $rootScope, $interval, $http, Rasa_Version, Sett
     });
   }
 
-  $scope.resendMessage = function() {
+  $scope.resendMessage = function () {
     $scope.test_text = $scope.selected_message.text;
     $scope.executeCoreRequest();
   }
@@ -107,13 +107,21 @@ function ChatController($scope, $rootScope, $interval, $http, Rasa_Version, Sett
 
   function checkForActions(messages_response) {
     if (messages_response.confidence && messages_response.confidence >= 1) {
-      var body = {'conversation_id': $scope.selected_conversation.conversation_id, action : {'name': messages_response.scores[0].action }};
+      var body = { 'conversation_id': $scope.selected_conversation.conversation_id, action: { 'name': messages_response.scores[0].action } };
       $http.post(appConfig.api_endpoint_v2 + '/rasa/conversations/execute', JSON.stringify(body)).then(function (response) {
         if (response.data && response.data.tracker) {
-          $scope.selected_conversation.conversation = JSON.stringify(response.data);
-          $scope.transactions = response.data.tracker.events;
-          $scope.loadConversationStory($scope.selected_conversation.conversation_id);
-          scrollToMessage();
+            var typing = {};
+            typing.event = "bot"
+            typing.text = " .... ";
+            $scope.transactions.push(typing);
+            
+            $timeout(function () {
+              $scope.transactions.pop();
+              $scope.selected_conversation.conversation = JSON.stringify(response.data);
+              $scope.transactions = response.data.tracker.events;
+              $scope.loadConversationStory($scope.selected_conversation.conversation_id);
+              scrollToMessage();
+            }, 1000);
         }
       },
         function (errorResponse) {
